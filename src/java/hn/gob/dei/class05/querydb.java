d296775a50d321559ffaa562cad52784f76eb2c4
Carlos Roberto Arias <cariasa@unitec.edu>
2016-08-15 20:41:43 -0600

    Versión 1, con respuesta, sin base de datos

---
+++ src/java/hn/gob/dei/class05/querydb.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.gob.dei.class05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Arias
+ */
+public class querydb extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Consultar la Base de Datos</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Consultar Empleados</h1>");
+            out.println("<body  bgcolor=\"#ffffff\">"
+                    + "<form method=\"get\">"
+                    + "<h2>Consultar empleados con nombre que empieza en:</h2>"
+                    + "<input title=\"Nombre: \" type=\"text\" "
+                    + "name=\"name\" size=\"25\"/>"
+                    + "<p></p>"
+                    + "<input type=\"submit\" value=\"Submit\"/>"
+                    + "<input type=\"reset\" value=\"Reset\"/>"
+                    + "</form>");
+            
+            String username = request.getParameter("name");
+            if (username != null && username.length() > 0) {
+                RequestDispatcher dispatcher =
+                        getServletContext().getRequestDispatcher("/response");
+                
+                if (dispatcher != null) {
+                    dispatcher.include(request, response);
+                }
+            }
+            out.println("</body></html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/hn/gob/dei/class05/response.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.gob.dei.class05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Arias
+ */
+public class response extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            String name = request.getParameter("name");
+            if (name != null && name.length() > 0) {
+                out.println("<h2>Hello, " + name + "!</h2>");
+            }
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Responde el query SELECT * FROM employees WHERE first_name LIKE param%";
+    }
+
+}
